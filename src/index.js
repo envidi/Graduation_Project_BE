24af99f4084328616325eb7fd311e97b1ddcd1ad
toilanome <namntph26878@fpt.edu.vn>
2023-12-13 15:22:22 +0700

    Feature signup signin

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
+import routerInit from './routes'
+import mongoose from 'mongoose'
 dotenv.config()
 const app = express()
 const { PORT } = process.env
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use('/api', routerInit) 
 
 app.use(express.json())
-
+mongoose.connect(`mongodb+srv://admin:123@cluster0.kczgtan.mongodb.net/?retryWrites=true&w=majority`).then(() =>{
+  console.log("Db kết nối thành công");
+})
 app.use(express())
 function start() {
   app.listen(PORT, () => {
