bc518edbc0a45e4b02726c2cb7e557dea818c1a4
envidi <phanm711996@gmail.com>
2023-12-21 14:07:26 +0700

    Validate category

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import express from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
@@ -6,7 +7,7 @@ import routerInit from './routes/index.js'
 import connectDB from './config/connect.js'
 dotenv.config()
 const app = express()
-const { PORT, DATABASE_API } = process.env
+const { PORT, DATABASE_API, HOST_NAME } = process.env
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -19,10 +20,11 @@ async function start() {
   try {
     await connectDB(DATABASE_API)
     app.listen(PORT, () => {
-      console.log(`http://localhost:${PORT}`)
+      console.log(`http://${HOST_NAME}:${PORT}`)
     })
   } catch (error) {
     console.log(error)
+    process.exit(0)
   }
 }
 start()
