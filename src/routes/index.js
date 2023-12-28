d87ada4f1999da92b396da3da9ce0799a62c4c99
quagsulu <quangnvph24106@fpt.edu.vn>
2023-12-28 22:34:13 +0700

    CRUD Cinema

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,14 @@ import routerProducts from './movie.js';
 import routerCategory from './category.js';
 import routerUser from './user.js';
 import { Router } from 'express';
+import routerCinema from './cinema.js';
 
 const routerInit = Router();
 
 routerInit.use('/user', routerUser )
 routerInit.use('/product', routerProducts )
 routerInit.use('/category', routerCategory )
+routerInit.use('/cinema', routerCinema )
 
 
-export default routerInit;
+export default routerInit; 
