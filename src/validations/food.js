9d2be40f8fbff26e9c21a977c6d7e5ffba073a1b
envidi <phanm711996@gmail.com>
2024-01-30 15:01:00 +0700

    chore ( ticket ) : Thêm ràng buộc khi xóa một food

--- src/validations/food.js
+++ src/validations/food.js
@@ -4,7 +4,6 @@ const foodValidationSchema = Joi.object({
   name: Joi.string().trim().required(),
   image: Joi.string().required(),
   price: Joi.number().min(0).required(),
-  quantity: Joi.number().min(0).required(),
   ticketId: Joi.array().items(Joi.string()),
   isDeleted: Joi.boolean()
 }).options({
