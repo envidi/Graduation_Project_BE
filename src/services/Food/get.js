ed83d89594e25e297e8d9d05678a70d6fc802d1e
nanh02 <anhnhi04102003@gmail.com>
2024-03-19 11:07:57 +0700

    fix (get all food)

--- src/services/Food/get.js
+++ src/services/Food/get.js
@@ -5,28 +5,24 @@ import ApiError from '../../utils/ApiError.js'
 
 export const getAllService = async (reqBody) => {
   try {
-    const {
-      _page = 1,
-      _limit = 10,
-      _sort = 'createdAt',
-      _order = 'asc',
-      includeDeleted // Thêm tham số này để kiểm tra query parameter
-    } = reqBody.query; // Sử dụng req.query thay vì req.body để nhận tham số từ query string
+    // const {
+    //   _sort = 'createdAt',
+    //   _order = 'asc',
+    //   includeDeleted // Thêm tham số này để kiểm tra query parameter
+    // } = reqBody.query; // Sử dụng req.query thay vì req.body để nhận tham số từ query string
 
-    const queryCondition = includeDeleted === 'true' ? {} : { isDeleted: false }
+    // const queryCondition = includeDeleted === 'true' ? {} : { isDeleted: false }
 
-    const options = {
-      page: _page,
-      limit: _limit,
-      sort: {
-        [_sort]: _order === 'asc' ? 1 : -1
-      }
-    }
+    // const options = {
+    //   sort: {
+    //     [_sort]: _order === 'asc' ? 1 : -1
+    //   }
+    // }
     // const data = await Food.paginate({}, options)
     // const data = await Food.paginate({ isDeleted: false }, options); // Chỉ lấy các thực phẩm chưa bị xóa mềm
-    const data = await Food.paginate(queryCondition, options);
+    const data = await Food.find({ isDeleted: false });
 
-    if (!data || data.docs.length === 0) {
+    if (!data || data.length === 0) {
       throw new ApiError(StatusCodes.NOT_FOUND, 'No food found!')
     }
     return data
