6df9c9f19ba96c2eeb9f8995889bf6322f20d307
envidi <phanm711996@gmail.com>
2024-04-07 15:57:58 +0700

    CORS

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.json())
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use('/api', routerInit)
 // Middleware xử lý lỗi tập trung
