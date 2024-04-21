fa488d058fc0a3a92e861dba3a812d5e84f5ea02
quagsulu <quangnvph24106@fpt.edu.vn>
2024-04-22 00:46:59 +0700

    fix : sủa lại chức năng giá

--- src/services/Movie/patch.js
+++ src/services/Movie/patch.js
@@ -11,12 +11,14 @@ import findDifferentElements from '../../utils/findDifferent.js'
 import { create, update } from '../../controllers/MoviePrice.js'
 import { moviePriceService } from '../moviePrice.js'
 import MoviePrice from '../../model/MoviePrice.js'
+import cloudinary from '../../middleware/multer.js'
 
 
 export const updateService = async (req) => {
   try {
     const id = req.params.id
     const body = req.body
+    delete body.avatar
     const { error } = movieSchema.validate(body, { abortEarly: true })
     if (error) {
       throw new ApiError(StatusCodes.BAD_REQUEST, new Error(error).message)
@@ -25,10 +27,23 @@ export const updateService = async (req) => {
     if (!checkmovie || checkmovie.length === 0) {
       throw new ApiError(StatusCodes.NOT_FOUND, 'No movies found! (k cj)')
     }
+    let imageUrl
+    let cloudGetUrl
+    if (req.file) {
+      cloudGetUrl = await cloudinary.uploader.upload(req.file.path, {
+        folder: 'AVATAR',
+        allowed_formats: ['jpg', 'png', 'jpeg'],
+        transformation: [{ width: 500, height: 500, crop: 'limit' }]
+      })
+      imageUrl = cloudGetUrl.secure_url
+    } else {
+      imageUrl = checkmovie.image
+    }
 
     // check suat chieu nếu có thì k sửa dc
     const checkshowtimes = await Showtimes.find({ movieId: id })
     const showtime = await checkshowtimes[0]
+    
     // if (showtime != undefined) {
     //   // if (checkmovie.duration != req.body.duration) {
     //   //   throw new ApiError(StatusCodes.NOT_FOUND, 'Phim đang xuất chiếu không thể sửa duration !')
@@ -52,37 +67,22 @@ export const updateService = async (req) => {
     // }
 
     // check destroy nếu đang xóa mềm thì không thể sửa được bất cứ trường nào
-    if (checkmovie.destroy == true) {
-      throw new ApiError(
-        StatusCodes.NOT_FOUND,
-        'The movie is in the deletion list and cannot be edited !'
-      )
-    }
+    // if (checkmovie.destroy == true) {
+    //   throw new ApiError(
+    //     StatusCodes.NOT_FOUND,
+    //     'The movie is in the deletion list and cannot be edited !'
+    //   )
+    // }
 
     // const data = await Movie.findByIdAndUpdate(id, body, { new: true })
     const data = await Movie.findById(id, 'categoryId')
-    // const dataprice1 = await MoviePrice.findById(checkmovie.prices[0])
-    // const dataprice2 = await MoviePrice.findById(checkmovie.prices[1])
-    // if (dataprice1) {
-    //   await update(checkmovie.prices[0], body.prices[0].price)
-    //   //  body.prices[0] = checkmovie.prices[0]
-    // } else {
-    //   await create({ movieId: id, price: body.prices[0].price, daytype: "weekday" })
-    // }
-    // if (dataprice2) {
-    //   // body.prices[1] = checkmovie.prices[1]
-    //   await update(checkmovie.prices[0], body.prices[1].price)
-    // } else {
-    //   await create({ movieId: id, price: body.prices[1].price, daytype: "weekend" })
-    // }
-
-    // const { prices, ...reqbody } = body
-
-
     const updateData = await Movie.updateOne(
       { _id: id },
       {
         ...body,
+      // ...(cloudGetUrl && { image: imageUrl }),
+      image: imageUrl,
+
         // ...reqbody,
         fromDate: new Date(convertTimeToIsoString(body.fromDate)),
         toDate: new Date(convertTimeToIsoString(body.toDate))
