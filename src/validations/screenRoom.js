1eaeb1c6cfadc13350d4cd04521d22364f062b2e
envidi <phanm711996@gmail.com>
2024-04-24 22:12:22 +0700

    fix(screenroom) : Sửa cập nhật phòng chiếu

--- src/validations/screenRoom.js
+++ src/validations/screenRoom.js
@@ -6,9 +6,11 @@ const screenSchema = Joi.object({
   name: Joi.string().required().min(6).max(255).label('Name').messages({
     'string.empty': `{{ #label }} is 'required'`
   }),
-  NumberSeat : Joi.number(),
-  projector :Joi.string().valid(...projectors).required(),
-  status : Joi.string().valid(...statusScreen),
+  NumberSeat: Joi.number().valid(...[49, 64, 72]),
+  projector: Joi.string()
+    .valid(...projectors)
+    .required(),
+  status: Joi.string().valid(...statusScreen),
   CinemaId: Joi.string().allow('').trim().strict(),
   ShowtimesId: Joi.array().items(Joi.string()).min(0),
   destroy: Joi.boolean()
